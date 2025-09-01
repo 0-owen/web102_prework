81973729156ff7a0c48751320e3018214897a58b
0-owen <owenhuang0819@gmail.com>
2025-09-01 18:24:38 -0400

    Add top funded and runner up stats

--- index.js
+++ index.js
@@ -188,7 +188,15 @@ const sortedGames = GAMES_JSON.sort((item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [firstGame, ...others] = sortedGames;
+let [secondGame, ...rest] = others; 
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameP = document.createElement("p");
+firstGameP.textContent = firstGame.name;
+firstGameContainer.append(firstGameP);
 
 // do the same for the runner up item
+const secondGameP = document.createElement("p");
+secondGameP.textContent = secondGame.name;
+secondGameContainer.append(secondGameP);
